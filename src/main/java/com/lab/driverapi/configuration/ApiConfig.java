fe9a0f81a274098eb7be0a374543863a586f8678
Fanuel Nunes <fnunes@testreach.com>
2025-02-21 17:33:37 -0300

    chore

--- src/main/java/com/lab/driverapi/configuration/ApiConfig.java
+++ src/main/java/com/lab/driverapi/configuration/ApiConfig.java
@@ -1,20 +1,8 @@
 package com.lab.driverapi.configuration;
 
-import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApiConfig {
-
-    private final TimeLimiterRegistry timeLimiterRegistry;
-
-    @PostConstruct
-    public TimeLimiterRegistry crate() {
-        System.out.println(timeLimiterRegistry);
-        return timeLimiterRegistry;
-    }
-}
+public class ApiConfig {}
