b4176ec5b22cef879b1dc09f2e7670ec7a01ca1a
Fanuel Nunes <fnunes@testreach.com>
2025-02-23 19:52:37 -0300

    attach app ao APM do kibana

--- src/main/java/com/lab/driverapi/configuration/ApiConfig.java
+++ src/main/java/com/lab/driverapi/configuration/ApiConfig.java
@@ -1,8 +1,32 @@
 package com.lab.driverapi.configuration;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
+import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApiConfig {}
+public class ApiConfig {
+
+  @Value("${elastic.apm.service_name}")
+  private String serviceName;
+
+  @Value("${elastic.apm.server_url}")
+  private String serverUrl;
+
+  @Value("${elastic.apm.application_packages}")
+  private String applicationPackages;
+
+  @PostConstruct
+  public void init() {
+    Map<String, String> apmProps = new HashMap<>();
+    apmProps.put("service_name", serviceName);
+    apmProps.put("server_url", serverUrl);
+    apmProps.put("application_packages", applicationPackages);
+    ElasticApmAttacher.attach(apmProps);
+  }
+}
